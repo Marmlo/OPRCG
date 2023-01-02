100ffbc5670e4a82821f754276bfa30435aa74b1
Stefan Šućur <stefansucur0@gmail.com>
2023-01-02 15:19:12 +0100

    Add jsons and name field

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,51 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import * as character from './character'
+import './style.css';
+/*import * as character from './character';*/
+
+import racesJson from '/src/data/races.json';
+var races = racesJson.list;
+var raceWeights = racesJson.weights;
+import affiliationsJson from '/src/data/affiliations.json';
+console.log(affiliationsJson);
+var affiliations = affiliationsJson.main.list;
+var affiliationWeights = affiliationsJson.main.weights;
+
+// Page building ---------------------------------------------------------------
+
+function makeButtons() {
+    // For now, only making the Gen All button
+    // Later make buttons in a for loop maybe
+    var button = document.createElement('button');
+    var genAllDiv = document.getElementById('gen-all-button');
+    button.className = 'text-white bg-orange-700 hover:bg-orange-300 hover:text-black hover:border-3 hover:border-orange-700 focus:border-3 focus:border-orange-400 focus:outline-0';
+    button.innerHTML = 'Generate All';
+    button.onclick = function() {
+        genAll();
+        this.blur(); //removes focus from the button (focus is opposite of blur)
+    }
+    genAllDiv.appendChild(button);
+}
+
+// Functions for the generation ------------------------------------------------
+
+function weightedRandom(items, weights) {
+    var i;
+
+    for (i = 0; i < weights.length; i++) {
+        weights[i] += weights[i - 1] || 0;
+    }
+
+    var random = Math.random() * weights[weights.length - 1];
+    
+    for (i = 0; i < weights.length; i++) {
+        if (weights[i] > random) {
+            break;
+        }
+    }
+    
+    return items[i];
+}
+
+// Generator functions ---------------------------------------------------------
 
 function genAll() {
     // Probably can be done with a for loop?
@@ -35,20 +80,8 @@ function genAll() {
     addbounty3();
 }
 
-function makeButtons() {
-    // For now, only making the Gen All button
-    // Later make buttons in a for loop maybe
-    var button = document.createElement('button');
-    var genAllDiv = document.getElementById('gen-all-button');
-    button.className = 'text-white bg-orange-700 hover:bg-orange-300 hover:text-black hover:border-3 hover:border-orange-700 focus:border-3 focus:border-orange-400 focus:outline-0';
-    button.innerHTML = 'Generate All';
-    button.onclick = function() {
-        doLetter(this.innerHTML);
-        this.blur();
-    }
-    genAllDiv.appendChild(button);
+function raceGen() {
+    document.getElementById("race").innerHTML = weightedRandom(races, raceWeights);
 }
 
-makeButtons()  
-
-
+makeButtons();
